ef09ebdf60b6cc08716a73ced3b0b56279fcdd4d
cocomong11 <carpediem1011@naver.com>
2025-11-18 16:01:29 +0900

    Update board.c

--- board.c
+++ board.c
@@ -9,7 +9,9 @@ void board_printBoardStatus(void);
 #define N_BOARD 30
 #define BOARDSSTATUS_OK 1
 #define BOARDSTATUS_NOK 0
-
+#define N_COINPOS 12
+#define MAX_COIN 4
+ 
 static int board_status[N_BOARD];
 static int board_coin[N_BOARD];
 static int shark_position;
@@ -29,4 +31,36 @@ void board_printBoardStatus(void)
 	}
 	printf("|\n");
 	printf("-----------------------------------------------\n");
+}
+int board_getBoardStatus(int pos);
+{
+	return board_status[pos];
+}
+int board_getBoardCOin(int pos)
+{
+	return board_coin[pos];
+}
+
+int board_initBoard(void)
+{
+    int i;
+    for (i = 0; i < N_BOARD; i++)
+    {
+        board_status[i] = 0;
+        board_coin[i] = 0;
+    }
+    for (i = 0; i < N_COINPOS; i++)
+    {
+        while (1)  
+        {
+            int pos = rand() % N_BOARD;   
+            if (board_coin[pos] == 0)   
+            {
+                board_coin[pos] = (rand() % MAX_COIN) + 1;  
+
+                break; 
+            }
+        }
+    }
+    return 0;
 }
